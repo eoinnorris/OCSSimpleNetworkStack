4bc8a4f063d1d6434d0f28c0885ea9cd56989893
eoinnorris <e.norris@mac.com>
2015-01-14 13:16:47 +0000

    Initial Commit

---
+++ OCSNetworkStack/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  OCSNetworkStack
+//
+//  Created by Eoin Norris on 14/01/2015.
+//  Copyright (c) 2015 Occassionaly Useful Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
